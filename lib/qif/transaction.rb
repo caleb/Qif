51bd47bc5005e81468589a6682bcafb3468cbe9b
John Scipione <jscipione@gmail.com>
2010-12-01 18:50:23 -0500

    running 'rake' now runs the tests, helpful that added check number to the fields list

--- lib/qif/transaction.rb
+++ lib/qif/transaction.rb
@@ -3,28 +3,30 @@ require 'qif/date_format'
 module Qif
   # The Qif::Transaction class represents transactions in a qif file.
   class Transaction
-    attr_accessor :date, :amount, :name, :description, :reference
-    
+    attr_accessor :date, :amount, :name, :description, :reference, :check_number
+
     def self.read(record) #::nodoc
       return nil unless record['D'].respond_to?(:strftime)
-      
+
       Transaction.new(
         :date => record['D'],
         :amount => record['T'].to_f, 
         :name => record['L'], 
         :description => record['M'],
-        :reference => record['P']
+        :reference => record['P'],
+        :check_number => record['N']
       )
     end
-    
+
     def initialize(attributes = {})
       @date = attributes[:date]
       @amount = attributes[:amount]
       @name = attributes[:name]
       @description = attributes[:description]
       @reference = attributes[:reference]
+      @check_number = attributes[:check_number]
     end
-    
+
     # Returns a representation of the transaction as it
     # would appear in a qif file.
     def to_s(format = 'dd/mm/yyyy')
@@ -33,7 +35,8 @@ module Qif
         'T' => '%.2f' % amount,
         'L' => name,
         'M' => description,
-        'P' => reference
+        'P' => reference,
+        'N' => check_number
       }.map{|k,v| "#{k}#{v}" }.join("\n")
     end
   end
--- spec/lib/transaction_spec.rb
+++ spec/lib/transaction_spec.rb
@@ -9,7 +9,8 @@ describe Qif::Transaction do
         'T' => '10.0',
         'L' => 'Credit',
         'M' => 'Supermarket',
-        'P' => 'abcde'
+        'P' => 'abcde',
+        'N' => '1001'
       )
       t.should be_a(Qif::Transaction)
       t.date.should == date
@@ -17,13 +18,14 @@ describe Qif::Transaction do
       t.name.should == 'Credit'
       t.description.should == 'Supermarket'
       t.reference.should == 'abcde'
+      t.check_number.should == '1001'
     end
-    
+
     it 'should return nil if the date does not respond to strftime' do
       Qif::Transaction.read('D' => 'hello').should be_nil
     end
   end
-  
+
   describe '#to_s' do
     before do
       @instance = Qif::Transaction.new(
@@ -31,29 +33,35 @@ describe Qif::Transaction do
         :amount => -10.0,
         :name => 'Debit',
         :description => 'Supermarket',
-        :reference => 'abcde'
+        :reference => 'abcde',
+        :check_number => '1001'
       )
     end
-    
+
     it 'should format the date in the format specified as D' do
       @instance.to_s('dd/mm/yyyy').should include('D02/01/2010')
       @instance.to_s('mm/dd/yyyy').should include('D01/02/2010')
     end
-    
+
     it 'should put the amount in T' do
       @instance.to_s.should include('T-10.00')
     end
-    
+
     it 'should put the name in L' do
       @instance.to_s.should include('LDebit')
     end
-    
+
     it 'should put the description in M' do
       @instance.to_s.should include('MSupermarket')
     end
-    
+
     it 'should put the reference in P' do
       @instance.to_s.should include('Pabcde')
     end
+
+    it 'should put the check number in N' do
+      @instance.to_s.should include('1001')
+    end
   end
 end
+
